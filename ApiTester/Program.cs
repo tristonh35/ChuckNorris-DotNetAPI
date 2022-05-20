f0148d42b96cafc91bb914aa82e5aac5ff2bc859
Triston H <87443056+tristonh35@users.noreply.github.com>
2022-05-19 18:54:45 -0700

    Initial commit

---
+++ ApiTester/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+using ChuckNorrisAPI;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ApiTester
+{
+    class Program
+    {
+        static void Main()
+        {
+            Run().Wait();
+        }
+
+        private static async Task Run()
+        {
+            //get random joke test
+            WriteLine("Getting a single joke**********************");
+            Joke j = await ChuckNorrisClient.GetRandomJoke();
+            WriteLine($"{j.Id}:{j.JokeText}\n\n{string.Join(", ", j.Categories)}");
+
+            //get categories
+            WriteLine("Getting categories****************************");
+            IEnumerable<string> categories = await ChuckNorrisClient.GetCategories();
+            foreach (var cat in categories)
+            {
+                WriteLine(cat);
+            }
+
+            //print specific joke
+            WriteLine("Getting specific joke***************************");
+            Joke j2 = await ChuckNorrisClient.GetJokeById(2);
+            WriteLine(j2.JokeText);
+
+            //retrieve joke that doesn't exist
+            WriteLine("Retrieving non-existent joke********************");
+            try
+            {
+                Joke j3 = await ChuckNorrisClient.GetJokeById(20000000);
+            }
+            catch (NoSuchQuoteException)
+            {
+                WriteLine("There is no joke/quote with that id");
+            }
+
+
+            ReadKey();
+        }
+    }
+}
---
+++ ChuckNorrisAPI/ChuckNorrisClientClasses.cs
@@ -0,0 +1,116 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisAPI
+{
+    public class ChuckNorrisClient
+    {
+        private static readonly HttpClient client;
+
+        static ChuckNorrisClient()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://api.icndb.com");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async static Task<Joke> GetRandomJoke()
+        {
+            HttpResponseMessage response = await client.GetAsync("jokes/random");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = JsonConvert.DeserializeObject<SingleJokeResponse>(await response.Content.ReadAsStringAsync());
+                var joke = data.JokeData;
+                joke.JokeText = WebUtility.HtmlDecode(joke.JokeText);
+
+                return data.JokeData;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async static Task<IEnumerable<Joke>> GetRandomJokes(int numJokes)
+        {
+            if (numJokes < 1)
+                throw new ArgumentException($"{nameof(numJokes)} must be a positive number");
+
+            HttpResponseMessage response = await client.GetAsync($"jokes/random/{numJokes}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = JsonConvert.DeserializeObject<MultiJokeResponse>(await response.Content.ReadAsStringAsync());
+                var allJokes = data.Jokes;
+
+                for (int i = 0; i < allJokes.Count(); i++)
+                {
+                    allJokes.ElementAt(i).JokeText = WebUtility.HtmlDecode(allJokes.ElementAt(i).JokeText);
+                }
+
+                return allJokes;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets a joke with a specific id
+        /// </summary>
+        /// <param name="id">The id of the joke to look for</param>
+        /// <exception cref="NoSuchQuoteException">Requested quote/joke does not exist</exception>
+        /// <returns></returns>
+        public async static Task<Joke> GetJokeById(int id)
+        {
+            HttpResponseMessage response = await client.GetAsync($"jokes/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                try
+                {
+                    var data = JsonConvert.DeserializeObject<SingleJokeResponse>(result);
+                    var joke = data.JokeData;
+                    joke.JokeText = WebUtility.HtmlDecode(joke.JokeText);
+
+                    return data.JokeData;
+                }
+                catch(JsonSerializationException)
+                {
+                    //this is used when a joke/quote with a specific id cannot be retrieved
+                    GeneralResponse generalResponse = JsonConvert.DeserializeObject<GeneralResponse>(result);
+
+                    if (generalResponse.Type != "success")
+                        throw new NoSuchQuoteException($"{generalResponse.Type}. A joke with an id of {id} does not exist");
+                }
+            }
+
+            return null;
+        }
+
+        public async static Task<IEnumerable<string>> GetCategories()
+        {
+            HttpResponseMessage response = await client.GetAsync($"categories");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = JsonConvert.DeserializeObject<CategoriesResponse>(await response.Content.ReadAsStringAsync());
+                return data.Categories;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
